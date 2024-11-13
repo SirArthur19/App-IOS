24a1b858dd62c095180753c7dba46d22044a1f2c
Arturo Anticona <artant0315@gmail.com>
2024-11-12 19:37:07 -0500

    Initial Commit

---
+++ cibertecapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cibertecapp
+//
+//  Created by DAMII on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
